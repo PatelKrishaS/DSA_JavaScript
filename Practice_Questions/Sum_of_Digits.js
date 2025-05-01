7bfb539cda83bdfee0cced5bed292ed24b075b40
PatelKrishaS <patelkrishasp@gmail.com>
2025-05-01 16:08:24 +0530

    sum of digits

---
+++ Practice_Questions/Sum_of_Digits.js
@@ -0,0 +1,33 @@
+//Sum of N natural numbers
+
+var pr = prompt("Enter the number till you want the summation of natural numbers: ")
+
+if (pr === null) {
+    console.log("Cancelled")
+}
+else {
+    var n = Number(pr)
+
+    if (isNaN(n)) {
+        console.log("Invalid Input");
+
+    }
+    else {
+        if (n > 0) {
+            var sum = 0;
+           while(n > 0){
+            var rem = n % 10;
+            sum = sum + rem;
+            n = Math.floor(n/10);
+           }
+            console.log(sum);
+
+
+        }
+        else {
+            console.log("The number should be positive and more than 0");
+
+        }
+    }
+}
+
